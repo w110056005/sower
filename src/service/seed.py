aa74138cec86cce090b2cefdea679643172bd2cc
Leon Liu <w110056005@mail.nchu.edu.tw>
2023-05-15 20:24:47 +0800

    Add ZeroMQ Client

--- src/service/seed.py
+++ src/service/seed.py
@@ -1,6 +1,12 @@
-import time
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect("tcp://localhost:5555")
+socket.setsockopt(zmq.SUBSCRIBE, b"")
+
+print("Running Sower service in background...")
 
 while True:
-    # Do something in the background
-    print("Running background service...")
-    time.sleep(5)
+    message = socket.recv_string()
+    print("Received message: %s" % message)

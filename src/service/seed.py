4e9892ac29ecd902c8fd9453439269613737452e
Leon Liu <w110056005@mail.nchu.edu.tw>
2023-05-31 16:55:38 +0800

    Add flower usage

--- src/service/seed.py
+++ src/service/seed.py
@@ -1,12 +1,30 @@
 import zmq
+import subprocess
 
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5555")
-socket.setsockopt(zmq.SUBSCRIBE, b"")
 
 print("Running Sower service in background...")
 
+def execute_python_file(file_path):
+    try:
+        # Execute the Python file as a separate process
+        subprocess.run(['python', file_path], check=True)
+    except subprocess.CalledProcessError as e:
+        # Handle any errors that occur during the execution
+        print(f"Error executing {file_path}: {e}")
+
+# Create a ZeroMQ context
+context = zmq.Context()
+# Create a subscriber socket
+socket = context.socket(zmq.SUB)
+# Set the subscription filter (empty string means subscribe to all messages)
+socket.setsockopt_string(zmq.SUBSCRIBE, '')
+# Connect to the publisher's address
+socket.connect('tcp://sower_platform_container:5555')
+
 while True:
+    print('Listening')
     message = socket.recv_string()
-    print("Received message: %s" % message)
+    print(f'Received message: {message}')
+    
+    if(message == "Start"):
+        execute_python_file("client.py")
--- src/sower_platform/website/views.py
+++ src/sower_platform/website/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import zmq
+import subprocess
+
+# Create a ZeroMQ context
+context = zmq.Context()
+# Create a publisher socket
+socket = context.socket(zmq.PUB)
+# Bind the publisher socket to a specific address
+socket.bind("tcp://0.0.0.0:5555")
+
+def execute_python_file(file_path):
+    try:
+        # Execute the Python file as a separate process
+        subprocess.run(['python', file_path], check=True)
+    except subprocess.CalledProcessError as e:
+        # Handle any errors that occur during the execution
+        print(f"Error executing {file_path}: {e}")
 
 # Create your views here.
 def hello(request):
+    print("Sending...")
+    message = "Start"
+    socket.send_string(message)
+    execute_python_file("./website/server.py")
     return HttpResponse("Hello, world!")
 

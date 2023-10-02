ae2531a47ead4877c33b93164df16f2c261dbabf
Leon Liu <w110056005@mail.nchu.edu.tw>
2023-10-02 23:10:33 +0800

    update flower server

--- src/sower_platform/website/server.py
+++ src/sower_platform/website/server.py
@@ -3,4 +3,11 @@ import sys
 
 port = sys.argv[1]
 addr = "[::]:"+port
-fl.server.start_server(server_address=addr, config=fl.server.ServerConfig(num_rounds=3))
+strategy = fl.server.strategy.FedAvg(
+    fraction_fit=1.0,
+    fraction_evaluate=1.0,
+    min_fit_clients=3,
+    min_evaluate_clients=3,
+    min_available_clients=3)
+
+fl.server.start_server(strategy=strategy, server_address=addr, config=fl.server.ServerConfig(num_rounds=10))
